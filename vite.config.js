8f28239b79fe242d5453d6bc84bb14d11385edef
Gourav Thakur <gouravthakur9991@gmail.com>
2026-07-21 00:46:02 +0530

    fix build issue

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) {
+              return 'vendor-react';
+            }
+            if (id.includes('lucide-react')) {
+              return 'vendor-lucide';
+            }
+            return 'vendor-libs';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000
+  }
 })
+
